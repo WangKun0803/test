6125ace061f6bffa5ddc707b3e54857488902895
wk <wk@163.com>
2024-08-23 15:18:11 +0800

    第三次提交

--- src/main/java/com/wk/Day01Application.java
+++ src/main/java/com/wk/Day01Application.java
@@ -8,7 +8,8 @@ public class Day01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Day01Application.class, args);
-        System.out.println("hello");
+        System.out.println("ss");
+
     }
 
 }
